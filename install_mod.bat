3ff693aa9efea9c39677fce829f1c5958cc29725
javi <javimusama@gmail.com>
2024-12-26 12:58:10 +0100

    Create install_mod.bat

---
+++ install_mod.bat
@@ -0,0 +1,33 @@
+@echo off
+REM Get the global path where the script is executing from
+set "scriptPath=%~dp0"
+
+REM Ensure scriptPath has a trailing backslash
+if not "%scriptPath:~-1%"=="\" set "scriptPath=%scriptPath%\"
+
+REM Get paths
+for %%a in ("%scriptPath%\resources\") do set "modPath=%%~fa"
+for %%a in ("%scriptPath%..\..\resources\") do set "originalGamePath=%%~fa"
+
+REM Output the paths
+echo Script will copy the contents of: %modPath% into %originalGamePath%
+
+REM Verify if the directories exist
+if not exist "%modPath%" (
+    echo Error: Source directory "%modPath%" does not exist!
+    pause
+    exit /b 1
+)
+
+if not exist "%originalGamePath%" (
+    echo Error: Destination directory "%originalGamePath%" does not exist!
+    pause
+    exit /b 1
+)
+
+REM Copy all files and subdirectories recursively, overwriting existing files
+xcopy "%modPath%" "%originalGamePath%" /E /H /C /I /Y
+
+REM Notify the user of completion
+echo Files have been copied successfully!
+pause
